b904011fa4bd81c806daf828b505dab29f740d41
Filipi Gustavo Boltão <filipigustavoboltao@mini-de-filipi.lan>
2022-03-01 11:52:34 -0300

    estudos aprofundados sobre a casa do caralho

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,15 @@ import './App.css';
 import TaskContainer from './components/TaskContainer';
 import TaskListContainer from './components/TaskListContainer';
 
+/**
+ * - nomenclatura das funções
+ * - reutilização dos componentes (Container, Form, etc)
+ * - subir o estado da aplicação (source of truth)
+ * - HOC's
+ * - menos é mais
+ * - clean code
+ * - tipagem (TS ou propTypes)
+ */
 function App() {
   const [tasks, setTasks] = React.useState([]);
 
